c41ab7ef3b0e53fef5553b868b533b2ca661fd89
visha7malvi <vishal7malvi@gmail.com>
2023-03-12 18:59:31 +0530

    - Added Facade example

---
+++ StructuralPatterns/Facade.playground/Contents.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+// Complex subsystem
+class AudioPlayer {
+    func loadSong() {
+        print("Loading song...")
+    }
+    
+    func playSong() {
+        print("Playing song...")
+    }
+    
+    func pauseSong() {
+        print("Pausing song...")
+    }
+    
+    func stopSong() {
+        print("Stopping song...")
+    }
+}
+
+// Facade
+class AudioPlayerFacade {
+    private let audioPlayer = AudioPlayer()
+    
+    func playMusic() {
+        audioPlayer.loadSong()
+        audioPlayer.playSong()
+    }
+    
+    func stopMusic() {
+        audioPlayer.stopSong()
+    }
+    
+    func pauseMusic() {
+        audioPlayer.pauseSong()
+    }
+}
+
+// code
+let audioPlayerFacade = AudioPlayerFacade()
+audioPlayerFacade.playMusic() // "Loading song..." and "Playing song..."
+audioPlayerFacade.pauseMusic() // "Pausing song..."
+audioPlayerFacade.stopMusic() //  "Stopping song..."
